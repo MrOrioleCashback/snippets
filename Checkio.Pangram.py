2e581fc4ef9854519951a57a132066e657bcf8ea
MrOrioleCashback <BrooksCarmichael@gmail.com>
2016-02-27 01:37:59 -0800

    Update Checkio.Pangram.py

--- Checkio.Pangram.py
+++ Checkio.Pangram.py
@@ -10,9 +10,15 @@ def uses_all(word, string):
 	return True 
 
 """      
-A pangram (Greek:παν γράμμα, pan gramma, "every letter") or holoalphabetic sentence for a given alphabet is a sentence using every letter of the alphabet at least once. Perhaps you are familiar with the well known pangram "The quick brown fox jumps over the lazy dog".
+A pangram (Greek:παν γράμμα, pan gramma, "every letter") or 
+holoalphabetic sentence for a given alphabet is a sentence using
+ every letter of the alphabet at least once. Perhaps you are familiar 
+ with the well known pangram "The quick brown fox jumps over the lazy 
+ dog".
 
-For this mission, we will use the latin alphabet (A-Z). You are given a text with latin letters and punctuation symbols. You need to check if the sentence is a pangram or not. Case does not matter.
+For this mission, we will use the latin alphabet (A-Z). You are given 
+a text with latin letters and punctuation symbols. You need to check 
+if the sentence is a pangram or not. Case does not matter.
 
 Input: A text as a string.
 
@@ -23,7 +29,9 @@ Example:
 check_pangram("The quick brown fox jumps over the lazy dog.") == True
 check_pangram("ABCDEF.") == False
 
-How it is used: Pangrams have been used to display typefaces, test equipment, and develop skills in handwriting, calligraphy, and keyboarding for ages.
+How it is used: Pangrams have been used to display typefaces, test 
+equipment, and develop skills in handwriting, calligraphy, and 
+keyboarding for ages.
 
 Precondition:
 

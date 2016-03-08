dfe89f583b4ad107bf21dbba2f11210eac2dfc12
MrOrioleCashback <BrooksCarmichael@gmail.com>
2016-03-07 18:28:58 -0800

    Update TPCh12.py

--- TPCh12.py
+++ TPCh12.py
@@ -85,7 +85,7 @@ def print_directory(d):
     for last, first in d:
         print(first, last, d[last, first])
         
-print_directory(directory)        
+#print_directory(directory)        
 
 
 #http://www.greenteapress.com/thinkpython/code/structshape.py
@@ -95,34 +95,106 @@ print_directory(directory)
 #useful for tracking data type and structure or 
 #trying to identify the cause of shape errors 
 
+words = open('words.txt').read().split('\n')
 
 
+def anagram_finder(word_list):
+    dict1 = {}
+    for word in word_list:
+        keyword = tuple(sorted(word))
+        if keyword in dict1:
+            dict1[keyword].append(word)
+        else:
+            dict1[keyword] = [word]
+    return sorted(dict1.values(), key=len, reverse=True)
+    
+#print(anagram_finder(words))
 
 
-###################
-# Rice U Quiz 6b  #
-# Questions 7 & 8 #
-###################
+def scrabble_bingo(word_list):
+    dict1 = {}
+    for word in word_list:
+        if len(word) == 8:
+            keyword = ''.join(sorted(word))
+            if keyword in dict1:
+                dict1[keyword].append(word)
+            else:
+                dict1[keyword] = [word]       
+    return max(dict1.values(), key=len)
 
-def question7(n):
-    numbers = range(2, n)
-    result = []
-    while len(numbers) > 0:
-        result.append(numbers.pop(0))
-        for index, x in enumerate(numbers):
-            if x % result[-1] == 0:
-                del numbers[index]
-    return len(result)            
-
-#print question7(1000)
+#print(scrabble_bingo(words))			
 
 
-def question8():
-    slow, fast, year = 1000, 1, 1
-    while slow > fast:
-        slow *= 1.2
-        fast *= 1.4
-        year += 1
-    return year   
-        
-#print question8()
+def metathesis_anagram_potentials(word_list):
+    dict1 = {}
+    for word in word_list:
+        keyword = tuple(sorted(word))
+        if keyword in dict1:
+            dict1[keyword].append(word)
+        else:
+            dict1[keyword] = [word]
+    return [x for x in dict1.values() if len(x)>1]  
+
+def metathesis_checker(anagram_list):
+	list1 = []
+	for collection in anagram_list:
+		for word1 in collection:
+			for word2 in collection:
+				counter = 0
+				for a, b in zip(word1, word2):
+					if a != b and counter < 3:
+						counter += 1
+				if counter == 2:
+					if (word2, word1) not in list1:
+						list1.append((word1, word2))
+	return sorted(list1)
+
+#print(metathesis_checker(metathesis_anagram_potentials(words)))
+
+"""
+What is the longest English word, that remains a valid English word, as you 
+remove its letters one at a time?
+Now, letters can be removed from either end, or the middle, but you can’t 
+rearrange any of the letters. Every time you drop a letter, you wind up with 
+another English word. If you do that, you’re eventually going to wind up with 
+one letter and that too is going to be an English word—one that’s found in the 
+dictionary. I want to know what’s the longest word and how many letters does 
+it have?
+I’m going to give you a little modest example: Sprite. Ok? You start off with 
+sprite, you take a letter off, one from the interior of the word, take the r 
+away, and we’re left with the word spite, then we take the e off the end, we’re 
+left with spit, we take the s off, we’re left with pit, it, and I.
+
+Write a program to find all words that can be reduced in this way, and then find 
+the longest one.
+"""
+
+def letter_chipper_boolen(word):
+	if len(word) == 1:
+		return word in words	
+	for n in range(len(word)):
+		chipped_word = (word[:n] + word[(n+1):])
+		if chipped_word in words:
+			return letter_chipper_boolen(chipped_word)
+	return False			
+
+def letter_chipper(word):
+	if len(word) == 1:
+		return word
+	for n in range(len(word)):
+		chipped_word = (word[:n] + word[(n+1):])
+		if chipped_word in words:
+			return word, letter_chipper(chipped_word)
+
+def cartalk_recursive_words(word_list):
+	for word in sorted(word_list, key=len, reverse=True): #check longest words first
+		if letter_chipper_boolen(word): #first True word encountered is returned
+			return letter_chipper(word) 
+
+print(cartalk_recursive_words(words))
+
+"""
+>>>
+('complecting', ('completing', ('competing', ('compting', 
+('comping', ('coping', ('oping', ('ping', ('pig', ('pi', 'i'))))))))))
+"""

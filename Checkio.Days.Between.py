b9b07f1da3762ab7029652ae37d8fc72e7fab572
MrOrioleCashback <BrooksCarmichael@gmail.com>
2016-03-27 20:00:21 -0700

    Update Checkio.Days.Between.py

--- Checkio.Days.Between.py
+++ Checkio.Days.Between.py
@@ -9,9 +9,15 @@ def days_diff(date1, date2):
         return 0
 
 """      
-How old are you in number of days? It's easy to calculate - just subtract your birthday from today. We could make this a real challenge though and count the difference between any dates.
+How old are you in number of days? It's easy to calculate - just subtract your 
+birthday from today. We could make this a real challenge though and count the 
+difference between any dates.
 
-You are given two dates as tuples with three numbers - year, month and day. For example: 19 April 1982 will be (1982, 4, 19). You should find the difference in days between the given dates. For example between today and tomorrow = 1 day. The difference will always be either a positive number or zero, so don't forget about absolute value.
+You are given two dates as tuples with three numbers - year, month and day. 
+For example: 19 April 1982 will be (1982, 4, 19). You should find the difference 
+in days between the given dates. For example between today and tomorrow = 1 day. 
+The difference will always be either a positive number or zero, so don't forget 
+about absolute value.
 
 Input: Two dates as tuples of integers.
 

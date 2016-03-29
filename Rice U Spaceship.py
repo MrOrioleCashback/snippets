50583838551e5157b04781004d1137dd5b1f3db9
MrOrioleCashback <BrooksCarmichael@gmail.com>
2016-03-29 02:53:43 -0700

    Update Rice U Spaceship.py

--- Rice U Spaceship.py
+++ Rice U Spaceship.py
@@ -2,6 +2,8 @@
 3/11/16
 Coursera, An Introduction to Interactive Programming in Python (Part 2), Rice University
 Mini-project #7 - 'Spaceship'
+
+copy code to http://www.codeskulptor.org/
 """
 
 import simplegui, random, math 

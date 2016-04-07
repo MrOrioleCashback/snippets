2dc7e4539009ab57b0f069e2d68908bc943d687a
MrOrioleCashback <BrooksCarmichael@gmail.com>
2016-04-07 14:07:01 -0700

    Update FlaskApp1.py

--- FlaskApp1.py
+++ FlaskApp1.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/') 	#event handler
+def index():		#event function
 	return '<h1>Hello World</h1>'
+	
+@app.route('/user/<name>') #dynamic route
+def user(name):
+	return '<h1>Hello, {}</h1>'.format(name)
 
-if __name__ == '__main__':
-	app.run(debug=True)
+if __name__ == '__main__':	#ensures dev server is started only
+	app.run(debug=True)	#when script is run directly
